7179651e791c7a1de0aeaf2c658424d62bf77882
julietalozano13 <julieta.lozano131004@gmail.com>
2026-04-28 21:49:11 -0600

    Update mailer.js

--- Backend/src/config/mailer.js
+++ Backend/src/config/mailer.js
@@ -1,15 +1,6 @@
 const emailjs = require("@emailjs/nodejs");
 
 async function sendOtpEmail({ to, fullName, otp }) {
-  if (
-    !process.env.EMAILJS_SERVICE_ID ||
-    !process.env.EMAILJS_TEMPLATE_ID ||
-    !process.env.EMAILJS_PUBLIC_KEY
-  ) {
-    console.log("EmailJS no configurado. OTP:", otp);
-    return;
-  }
-
   const templateParams = {
     email: to,
     to_email: to,
@@ -18,18 +9,18 @@ async function sendOtpEmail({ to, fullName, otp }) {
     appName: "TaskHub",
   };
 
-const result = await emailjs.send(
-  process.env.EMAILJS_SERVICE_ID,
-  process.env.EMAILJS_TEMPLATE_ID,
-  templateParams,
-  {
-    publicKey: process.env.EMAILJS_PUBLIC_KEY,
-  }
-);
+  const result = await emailjs.send(
+    process.env.EMAILJS_SERVICE_ID,
+    process.env.EMAILJS_TEMPLATE_ID,
+    templateParams,
+    {
+      publicKey: process.env.EMAILJS_PUBLIC_KEY,
+      privateKey: process.env.EMAILJS_PRIVATE_KEY,
+    }
+  );
 
-console.log("EmailJS result:", result);
-return result;
-
+  console.log("EmailJS result:", result);
+  return result;
 }
 
 module.exports = { sendOtpEmail };
